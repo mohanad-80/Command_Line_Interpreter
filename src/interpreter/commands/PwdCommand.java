142257a2883d6f06b0cbedf499438c585bc928fc
AhmedEhab2022 <am8122004@gmail.com>
2024-10-27 17:03:08 +0300

    update: pwd command to use context without set it

--- src/interpreter/commands/PwdCommand.java
+++ src/interpreter/commands/PwdCommand.java
@@ -5,7 +5,6 @@ import interpreter.InterpreterContext;
 
 public class PwdCommand {
     public String execute(Command command, InterpreterContext context) {
-        context.setCurrentDirectory(System.getProperty("user.dir"));
         return context.getCurrentDirectory().toString();
     }
 }
